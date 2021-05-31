4da8733e31a9503f6b8c9f53090171d66be0bb84
KamMis98 <81377976+KamMis98@users.noreply.github.com>
2021-05-31 21:58:46 +0200

    Update Aij.py

--- Aij.py
+++ Aij.py
@@ -1,12 +1,6 @@
 import numpy as np
 
 def Aij(df_i, df_j, c, f_i, f_j):
-    '''
-    Parametry :
-    df_i , df_j - pochodne funkcji ksztaltu zwiazanych z i-tym oraz j- tym wezlem
-    Zwraca :
-    wyrazenie lambda bedace funkcja podcalkowa A_ij
-    '''
     
     # fun_podc = lambda x: -df_i(x)*df_j(x) + c*f_i(x)*f_j(x) 
 
